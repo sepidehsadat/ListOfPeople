cd444cc4c7c5431f2a9e89248e4d22aca83cd108
Sepideh <ssmazlumi@gmail.com>
2023-09-01 19:43:18 +0330

    create list peopeles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-import { useState } from "react";
+import List from "./components/List";
 
 function App() {
+  const [peoples, setPeoples] = useState<
+    { fullName: string; age: number; img: string; bio?: string }[]
+  >([{ fullName: "sepideh", age: 27, img: "test", bio: "test" }]);
   return (
     <div className="App">
-      <p>تست</p>
+      {peoples.map((p) => (
+        <>{p.fullName}</>
+      ))}
     </div>
   );
 }
