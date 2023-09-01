1919b507cd449eca64b916397232bb32cafaa8eb
Sepideh <ssmazlumi@gmail.com>
2023-09-01 18:54:16 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <p>تست</p>
+    </div>
+  );
+}
+
+export default App;
