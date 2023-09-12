b569d8a3bd7ccda0e6cb6e528a31b8fb45284750
Sepideh <ssmazlumi@gmail.com>
2023-09-13 00:40:44 +0330

    textarea

---
+++ src/components/PTextArea.tsx
@@ -0,0 +1,51 @@
+
+
+import React from "react";
+import "./PTextArea.css";
+
+interface IProps
+{
+    title: string;
+    placeholder: string;
+}
+
+interface IState
+{
+    value: string;
+}
+
+export default class PTextArea extends React.Component<IProps, IState> {
+    constructor(props: IProps)
+    {
+        super(props);
+        this.state = {
+            value: "",
+        };
+        this.setValue = this.setValue.bind(this);
+        this.getValue = this.getValue.bind(this);
+    }
+    setValue(e: React.ChangeEvent<HTMLTextAreaElement>): void
+    {
+        console.log(e)
+        // this.setState({ value: e});
+    }
+    getValue(): string
+    {
+        return this.state.value;
+    }
+    override render()
+    {
+        return (
+            <div className="form">
+                <textarea
+                    className="p_text_area"
+                    onChange={ this.setValue}
+                    placeholder={this.props.placeholder}
+                />
+            </div>
+        );
+    }
+}
+
+
+
