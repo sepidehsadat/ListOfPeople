f7bf9ba86d92cf25e6f8f14aee1eae353a0708ca
Sepideh <ssmazlumi@gmail.com>
2023-09-16 18:45:29 +0330

    Create Server

---
+++ src/api/JAServer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "http://localhost:8080/api",
+    headers: {
+        "Content-type": "application/json"
+    }
+});
