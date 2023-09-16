c67a7a289ea6af2828ee82d0a2a36ac0da881877
Sepideh <ssmazlumi@gmail.com>
2023-09-16 23:53:58 +0330

    change Type

--- src/api/PServer.tsx
+++ src/api/PServer.tsx
@@ -1,15 +1,23 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+
+export interface PostType
+{
+    userId?: number;
+    id?: number;
+    title: string;
+    body: string;
+}
 
 const instance = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com/',
 });
 const server = {
-	get: (url: string) => instance.get(url).then(),
-	// post: (url: string, body: {}) => instance.post(url, body).then(),
-	// put: (url: string, body: {}) => instance.put(url, body).then(),
-	// delete: (url: string) => instance.delete(url).then(),
+    get: (url: string) => instance.get(url).then(),
+    // post: (url: string, body: {}) => instance.post(url, body).then(),
+    // put: (url: string, body: {}) => instance.put(url, body).then(),
+    // delete: (url: string) => instance.delete(url).then(),
 };
 
 export const requests = {
-	getData: (): Promise<any> => server.get(`posts`),
+    getData: (): Promise<AxiosResponse> => server.get(`posts`),
 };
